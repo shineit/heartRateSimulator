dfc31c6df54b1452c9de12884e9deb01c6c9e019
chipk215 <chipk215@msn.com>
2013-02-20 09:12:00 -0700

    Adding file

---
+++ HeartRateMonitorSimulator/PeripheralManagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PeripheralManagerViewController.h
+//  HeartRateMonitorSimulator
+//
+//  Created by Chip Keyes on 2/20/13.
+//  Copyright (c) 2013 Chip Keyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PeripheralManagerViewController : UIViewController
+
+@end
